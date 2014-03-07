59fc064d69c2bc723aac79da3ffc31dbd4ff1504
kongakong <anthony.hw.kong@gmail.com>
2014-03-07 17:48:53 +1100

    Initial Commit

---
+++ TestKBWithLabel/TKWLViewController.m
@@ -0,0 +1,29 @@
+//
+//  TKWLViewController.m
+//  TestKBWithLabel
+//
+//  Created by Anthony Kong on 7/03/2014.
+//  Copyright (c) 2014 Anthony Kong. All rights reserved.
+//
+
+#import "TKWLViewController.h"
+
+@interface TKWLViewController ()
+
+@end
+
+@implementation TKWLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TestKBWithLabel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestKBWithLabel
+//
+//  Created by Anthony Kong on 7/03/2014.
+//  Copyright (c) 2014 Anthony Kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TKWLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TKWLAppDelegate class]));
+    }
+}
