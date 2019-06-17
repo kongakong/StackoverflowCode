1e65c18a377215852ce8b79b776ff0fdada70a40
Anthony Kong <anthony@zeetings.com>
2019-06-17 16:48:27 +1000

    [python-aws-sign-v4] Compared with s3v4

--- python/boto3-sign-v4/scripts/gen_url.py
+++ python/boto3-sign-v4/scripts/gen_url.py
@@ -2,12 +2,16 @@
 
 # In a very simple version, the url generated is
 # https://zeetings-upload-play.s3.amazonaws.com/1-2-bcea83ce-22d4-4785-b066-f038fedb9787.pptx?AWSAccessKeyId=ABC&content-type=image%2Fjpeg&Expires=1560840279&Signature=ZJRC4QFMAXYCrauF%2FdTVlEjnnF8%3D
+# But with the s3v4 switch, it becomes
+# https://zeetings-upload-play.s3.amazonaws.com/1-2-bcea83ce-22d4-4785-b066-f038fedb9787.pptx?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Expires=86400&X-Amz-Credential=ABC%2F20190617%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-SignedHeaders=content-type%3Bhost&X-Amz-Date=20190617T064705Z&X-Amz-Signature=bf8c49c3de6bd8e40589e85b52e6fe25219fc76ea53031960c113dfd423562e6
 
 import boto3
+from botocore.client import Config
 from datetime import datetime, date, time
 
 def sign(access_key, secret_key, s3_bucket, object_name, mime_type, expires):
     s3_client = boto3.client('s3',
+                             config=Config(signature_version='s3v4'),
                              aws_access_key_id=access_key,
                              aws_secret_access_key=secret_key)
 
