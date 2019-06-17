82ba5b3682a53f30b26a1297346890325f52a8ee
Anthony Kong <anthony@zeetings.com>
2019-06-17 19:29:48 +1000

    [python-aws-sign-v4] Make flexible

--- python/boto3-sign-v4/scripts/gen_url.py
+++ python/boto3-sign-v4/scripts/gen_url.py
@@ -5,6 +5,7 @@
 # But with the s3v4 switch, it becomes
 # https://zeetings-upload-play.s3.amazonaws.com/1-2-bcea83ce-22d4-4785-b066-f038fedb9787.pptx?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Expires=86400&X-Amz-Credential=ABC%2F20190617%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-SignedHeaders=content-type%3Bhost&X-Amz-Date=20190617T064705Z&X-Amz-Signature=bf8c49c3de6bd8e40589e85b52e6fe25219fc76ea53031960c113dfd423562e6
 
+import sys
 import boto3
 from botocore.client import Config
 from datetime import datetime, date, time
@@ -28,9 +29,9 @@ def sign(access_key, secret_key, s3_bucket, object_name, mime_type, expires):
     return response
 
 
-def main():
-    key = 'ABC'
-    secret = 'BBC'
+def main(key, secret):
+    access_key = 'ABC' if key is None else key
+    secret_key = 'EDF' if secret is None else secret 
     s3_bucket = 'zeetings-upload-play'
     object_name = '1-2-bcea83ce-22d4-4785-b066-f038fedb9787.pptx'
     mime_type = 'image/jpeg'
@@ -42,5 +43,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    key = secret = None
+    if len(sys.argv) == 3:
+        key = sys.argv[1]
+        secret = sys.argv[2]
+
+    main(key, secret)
 
