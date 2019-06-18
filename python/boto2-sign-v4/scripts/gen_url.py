52635a2fb02714f5cee5005c01ae8aa59539655a
Anthony Kong <anthony@zeetings.com>
2019-06-18 13:25:55 +1000

    [python-aws-sign-v4] Enabled hmac-v4-s3 with environment variable

--- python/boto2-sign-v4/scripts/gen_url.py
+++ python/boto2-sign-v4/scripts/gen_url.py
@@ -6,14 +6,16 @@
 # https://zeetings-upload-play.s3.amazonaws.com/1-2-bcea83ce-22d4-4785-b066-f038fedb9787.pptx?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Expires=86400&X-Amz-Credential=ABC%2F20190617%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-SignedHeaders=content-type%3Bhost&X-Amz-Date=20190617T064705Z&X-Amz-Signature=bf8c49c3de6bd8e40589e85b52e6fe25219fc76ea53031960c113dfd423562e6
 
 import sys
+import os
 from datetime import datetime, date, time
 import boto
 from boto.s3.connection import S3Connection
 
 
 def sign(access_key, secret_key, bucket, path, mime_type, expiry, region):
+    os.environ['S3_USE_SIGV4'] = 'True'
     boto.set_stream_logger('boto')
-    c = S3Connection(access_key, secret_key)
+    c = S3Connection(access_key, secret_key, host='s3.amazonaws.com')
     return c.generate_url_sigv4(
                   expires_in=long(expiry),
                   method='PUT',
