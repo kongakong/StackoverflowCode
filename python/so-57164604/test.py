ecbd3f38968064a14beaf27483637826c9174e99
Anthony Kong <anthony@zeetings.com>
2019-07-24 11:35:04 +1000

    [SO-57164604.sklearn] Add the question

--- python/so-57164604/test.py
+++ python/so-57164604/test.py
@@ -5,7 +5,7 @@ from enum import Enum
 var1 = 'var1'
 var2 = 'var2'
 abc = [1, 2]
-xyz = list(range(1_00_000))
+xyz = list(range(1000))  # reduce numbers for debug
 pg = [{'variant': [var1],
        'abc': abc,
        'xyz': xyz, },
@@ -18,6 +18,7 @@ print('len(parameterGrid):', len(parameterGrid))
 
 
 def myFunc(myParam):
+    print("Start working")
     if myParam['abc'] == 1:
         raise ValueError('error thrown')
     print(myParam)
