ac497a7f32c9f5f1e45d5a79b4c9089102cabbc2
Anthony Kong <anthony@zeetings.com>
2020-03-28 12:35:25 +1100

    [so-60895313] first cut

---
+++ python/so-60895313-covid19/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly.graph_objects as go
+import requests
+from datetime import date, timedelta
+yesterday = date.today() - timedelta(days=1)
+
+
+confirmed_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+deaths_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+yesterdays_date = yesterday.strftime('%-m/%d/%y')
+
+confirmed = pd.read_csv(confirmed_url)
+deaths = pd.read_csv(deaths_url)
+confirmed.iloc[0]['Country/Region'] #Test
+
+for place in deaths[['Province/State','Country/Region']]:
+    if place is float:
+        deaths_names.append('Country/Region')
+    else:
+        deaths_names.append('Province/State')
+
+confirmed['Name'] = df(confirmed_names)
+deaths['Name'] = df(deaths_names)
