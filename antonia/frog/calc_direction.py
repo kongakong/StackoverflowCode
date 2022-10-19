03029976cb7119d7f025039a94adcbce2722bb6b
Anthony Kong <anthony@canva.com>
2022-10-19 20:15:41 +1100

    [antonia-is-a-frog] Final

--- antonia/frog/calc_direction.py
+++ antonia/frog/calc_direction.py
@@ -9,6 +9,23 @@ class Bug:
         self.x = x
         self.y = y
 
+class Tile:
+    def __init__(self, is_log=False, has_bug=False):
+        self.is_log = is_log
+        self.has_bug = has_bug
+
+    def __str__(self):
+        if self.has_bug:
+            return "B"
+        return "L" if self.is_log else "~"
+
+
+def print_board():
+    global game_board     
+    for rows in game_board:
+        row_line = ",".join(str(tile) for tile in rows)
+        print(row_line)
+    print("\n")
 
 def move_bug(bug, x_direction, y_direction, move_step):
     global game_board     
@@ -16,9 +33,9 @@ def move_bug(bug, x_direction, y_direction, move_step):
     old_y_pos = bug.y
     bug.x = bug.x + move_step * x_direction
     bug.y = bug.y + move_step * y_direction
-    game_board[old_y_pos][old_x_pos] = "L"
-    game_board[bug.y][bug.x] = "B"
-    pprint(game_board)
+    game_board[old_y_pos][old_x_pos].has_bug = False
+    game_board[bug.y][bug.x].has_bug = True 
+    print_board()
 
 
 
@@ -26,21 +43,22 @@ def main():
 
     global game_board 
     game_board = []
-    for i in range(0, 9):
-        game_board.append(["~"] * 9)
+    for y in range(0, 9):
+        board_row = []
+        for x in range(0, 9):
+            board_row.append(Tile(is_log=True if y == 7 else False))
+        game_board.append(board_row)
 
-    # set B at row 8, col 3
-    game_board[7] = ["L"] * 9
+    print_board()
 
+    # explain the directionality
     move_step = 1
     x_direction = 1  # 0 for no move, 1 for right, -1 for left
     y_direction = 0  # 0 for no move, 1 for down, -1 for up
 
     bug = Bug(y=7, x=2)
-    # print(bug.x, bug.y)
-    game_board[bug.y][bug.x] = "B"
-
-    pprint(game_board)
+    game_board[bug.y][bug.x].has_bug = True
+    print_board()
 
     # let move B to right
     print("Move to right (1)")
@@ -52,6 +70,9 @@ def main():
     
     print("Move up (-1)")
     move_bug(bug, 0, -1, 1)
+
+    move_bug(bug, 0, -1, 1)
+
 
 if __name__ == "__main__":
     main()
